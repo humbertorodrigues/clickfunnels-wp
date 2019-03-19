28cb4633d16a10b7ff8395809212d2604ceb82e2
Humberto Rodrigues <humberto.rodrigues@gmail.com>
2019-03-19 13:43:59 -0300

    First commit

---
+++ README.md
@@ -0,0 +1,12 @@
+# ClickFunnels-WP
+
+Import a clickfunnel page inside WP without changing your domain DNSs
+
+## Getting Started
+
+1 - Install as any wp plugin
+2 - Use the shortcode [clickfunnels url=""]. THe url must be filled with the clickfunnels page url.
+![Image from clickfunnels URL](https://i.imgur.com/ditIVf5.png)
+
+
+
---
+++ home.php
@@ -0,0 +1,36 @@
+<?php
+/**
+* Plugin Name: ClickFunnels-WP
+* Plugin URI: http://humberto.ninja
+* Description: Load clickfunnels pages inside wp
+* Version: 1.0
+* Author: Humberto Rodrigues
+
+**/
+add_shortcode( 'clickfunnels' , 'func_pegar_pagina' );
+add_filter( 'template_include', 'substituir_template',99);
+
+function func_pegar_pagina($atts){
+    $atts = shortcode_atts(array('url'=>''),$atts);
+    $url = $atts['url'];
+    $pagina = file_get_contents($url);
+    $pagina = str_ireplace("%5B","[",$pagina);
+    $pagina = str_ireplace("%5D","]",$pagina);
+    $pagina = do_shortcode( $pagina );
+    return $pagina;
+}
+
+function substituir_template( $template ) {
+	global $post;
+
+	if ( has_shortcode($post->post_content, 'clickfunnels' ) ) {
+		$conteudo = do_shortcode( $post->post_content );
+		$template =  (__DIR__."/template/template-structure.php");
+		echo $conteudo;
+	}else{
+	
+	}
+
+    return $template;
+}
+
